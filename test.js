a46f5ed6e0b37b5cdc1430615741ecd1cc9a2507
Voicescu Robert <voicescurobert22@stud.ase.ro>
2024-10-08 16:20:02 +0300

    variables

---
+++ test.js
@@ -0,0 +1,13 @@
+function test(){
+    if(true){
+        var var1 = 1;
+        const const2 = 2;
+        let let3 = 3;
+    }
+
+    console.log(var1);
+    console.log(const2);
+    console.log(let3);
+}
+
+console.log(var1);
